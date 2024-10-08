0835a66b129b925c330a6ca6e0baa724b1aa58be
Ashwin Tauro <ashwinshawntauro@gmail.com>
2024-10-08 15:34:32 +0530

    added theme, added shadcn, signup ui

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -1,3 +1,5 @@
 export default function Page() {
-  return <h1>Welcome to Signin!</h1>;
+  return (
+    <div>Signin</div>
+  );
 }
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,3 +1,48 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function Page() {
-  return <h1>Welcome to Signup!</h1>;
+  return (
+    <>  
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          {/* <Image className="mx-auto h-10 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/> */}
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register your account</h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <Label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</Label>
+              <div className="mt-2">
+                <Input id="email" name="email" type="email" autocomplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 sm:text-sm sm:leading-6"/>
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <Label for="password" className="block text-sm font-medium leading-6 text-gray-900">Password</Label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-primary">Forgot password?</a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <Input id="password" name="password" type="password" autocomplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"/>
+              </div>
+            </div>
+
+            <div>
+              <Button type="submit" className="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white">Register</Button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Already a member?
+            <a href="#" className="font-semibold leading-6 text-primary"> Sign In</a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
