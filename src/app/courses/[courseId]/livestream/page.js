3a10a94185e3ca8ea930ac1ff52ef8ef7d4b9aed
Ashwin Tauro <ashwinshawntauro@gmail.com>
2024-10-18 23:41:32 +0530

    style:updated livestream ui

--- src/app/courses/[courseId]/livestream/page.js
+++ src/app/courses/[courseId]/livestream/page.js
@@ -11,11 +11,12 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-// import Janus from 'janus-gateway'; 
+// import Janus from 'janus-gateway';
 // import adapter from "webrtc-adapter"
 
-export default function Page() {
+export default function Page({ params }) {
   const myVideoRef = useRef();
+  const [activeTab, setActiveTab] = useState("module1");
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState("");
   // const [janus, setJanus] = useState(null);
@@ -82,73 +83,376 @@ export default function Page() {
   };
 
   return (
-    <div className="flex flex-col justify-center p-12">
-      <h4 className="text-center">Live Streaming</h4>
-      <div className="grid stream-grid p-2">
-        <video
-          className="w-full h-full p-3 rounded-md" // Ensure rounded corners
-          playsInline
-          ref={myVideoRef}
-          autoPlay
-          muted
-        />
-        <div className="mt-4">
-          <Card className="w-[350px] h-[645px] flex flex-col">
-            <CardHeader>
-              <CardTitle>Live Chat</CardTitle>
-              <CardDescription>
-                Engage with your viewers in real-time.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="flex-1 overflow-y-auto">
-              {messages.length === 0 ? (
-                <div className="text-center text-gray-500">
-                  No messages yet. Be the first to chat!
-                </div>
-              ) : (
-                messages.map((msg, index) => (
-                  <div
-                    key={index}
-                    className="p-2 mb-2 bg-gray-100 rounded-md text-sm"
-                  >
-                    {msg}
-                  </div>
-                ))
-              )}
-            </CardContent>
-            <CardFooter className="mt-auto">
-              <div className="grid w-full gap-2">
-                <Textarea
-                  placeholder="Type your message here."
-                  value={input}
-                  onChange={(e) => setInput(e.target.value)}
-                />
-                <Button onClick={handleSendMessage}>Send message</Button>
+    <div className="grid main-live-grid">
+      {/* Left Panel */}
+      <div>
+        <button
+          data-drawer-target="default-sidebar"
+          data-drawer-toggle="default-sidebar"
+          aria-controls="default-sidebar"
+          type="button"
+          class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        >
+          <span class="sr-only">Open sidebar</span>
+          <svg
+            class="w-6 h-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              clip-rule="evenodd"
+              fill-rule="evenodd"
+              d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+            ></path>
+          </svg>
+        </button>
+        <div className="h-screen nav-live-grid grid">
+          {/* Sidebar with vertical Tabs */}
+          <aside className="bg-gray-100 p-4">
+            {/* Vertical Tab List */}
+            <div className="flex flex-col space-y-2">
+              <button
+                onClick={() => setActiveTab("module1")}
+                className={`w-full text-left py-2 px-4 rounded-lg ${
+                  activeTab === "module1"
+                    ? "bg-blue-500 text-white"
+                    : "hover:bg-gray-200 text-gray-700"
+                }`}
+              >
+                1
+              </button>
+              <button
+                onClick={() => setActiveTab("module2")}
+                className={`w-full text-left py-2 px-4 rounded-lg ${
+                  activeTab === "module2"
+                    ? "bg-blue-500 text-white"
+                    : "hover:bg-gray-200 text-gray-700"
+                }`}
+              >
+                2
+              </button>
+              <button
+                onClick={() => setActiveTab("module3")}
+                className={`w-full text-left py-2 px-4 rounded-lg ${
+                  activeTab === "module3"
+                    ? "bg-blue-500 text-white"
+                    : "hover:bg-gray-200 text-gray-700"
+                }`}
+              >
+                3
+              </button>
+            </div>
+          </aside>
+
+          {/* Module Contents */}
+          <div className="flex-grow p-6">
+            {activeTab === "module1" && (
+              <div>
+                <h3 className="text-lg text-center font-semibold mb-4">
+                  Module 1
+                </h3>
+                <hr></hr>
+                <ul className="space-y-2 font-medium">
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                    >
+                      <svg
+                        class="w-6 h-6 text-gray-800 dark:text-white"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M6 2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 1 0 0-2h-2v-2h2a1 1 0 0 0 1-1V4a2 2 0 0 0-2-2h-8v16h5v2H7a1 1 0 1 1 0-2h1V2H6Z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+
+                      <span className="ms-3">Notes 1</span>
+                    </a>
+                  </li>
+                </ul>
               </div>
-            </CardFooter>
-          </Card>
+            )}
+
+            {activeTab === "module2" && (
+              <div>
+              <h3 className="text-lg text-center font-semibold mb-4">
+                Module 2
+              </h3>
+              <hr></hr>
+              <ul className="space-y-2 font-medium">
+                <li>
+                  <a
+                    href="#"
+                    className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                  >
+                    <svg
+                      class="w-6 h-6 text-gray-800 dark:text-white"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M6 2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 1 0 0-2h-2v-2h2a1 1 0 0 0 1-1V4a2 2 0 0 0-2-2h-8v16h5v2H7a1 1 0 1 1 0-2h1V2H6Z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+
+                    <span className="ms-3">Notes 1</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                  >
+                    <svg
+                      class="w-6 h-6 text-gray-800 dark:text-white"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M6 2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 1 0 0-2h-2v-2h2a1 1 0 0 0 1-1V4a2 2 0 0 0-2-2h-8v16h5v2H7a1 1 0 1 1 0-2h1V2H6Z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+
+                    <span className="ms-3">Notes 2</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            )}
+
+            {activeTab === "module3" && (
+              <div>
+              <h3 className="text-lg text-center font-semibold mb-4">
+                Module 3
+              </h3>
+              <hr></hr>
+              <ul className="space-y-2 font-medium">
+                <li>
+                  <a
+                    href="#"
+                    className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                  >
+                    <svg
+                      class="w-6 h-6 text-gray-800 dark:text-white"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M6 2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 1 0 0-2h-2v-2h2a1 1 0 0 0 1-1V4a2 2 0 0 0-2-2h-8v16h5v2H7a1 1 0 1 1 0-2h1V2H6Z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+
+                    <span className="ms-3">Notes 1</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            )}
+          </div>
         </div>
       </div>
-      <Tabs defaultValue="agenda" className="w-full p-2 bg-gray-50">
-        <TabsList className="grid w-full grid-cols-4">
-          <TabsTrigger value="announcement">Announcement</TabsTrigger>
-          <TabsTrigger value="agenda">Agenda</TabsTrigger>
-          <TabsTrigger value="notes">Notes</TabsTrigger>
-          <TabsTrigger value="doubt">Raise a Doubt</TabsTrigger>
-        </TabsList>
-        <TabsContent value="announcement">
-          No Announcements from tutor yet!
-        </TabsContent>
-        <TabsContent value="agenda">
-          Live stream agenda will be displayed here.
-        </TabsContent>
-        <TabsContent value="notes">
-          Take down notes
-        </TabsContent>
-        <TabsContent value="doubt">
-          Ask a question from tutor
-        </TabsContent>
-      </Tabs>
+      {/* Right Panel */}
+      <div className="flex flex-col justify-center p-2">
+        {/* Breadcrumb */}
+        <nav
+          class="flex px-5 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
+          aria-label="Breadcrumb"
+        >
+          <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+            <li class="inline-flex items-center">
+              <a
+                href="#"
+                class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+              >
+                <svg
+                  class="w-3 h-3 me-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                </svg>
+                Home
+              </a>
+            </li>
+            <li>
+              <div class="flex items-center">
+                <svg
+                  class="rtl:rotate-180 block w-3 h-3 mx-1 text-gray-400 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+                <a
+                  href="#"
+                  class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white"
+                >
+                  Courses
+                </a>
+              </div>
+            </li>
+            <li aria-current="page">
+              <div class="flex items-center">
+                <svg
+                  class="rtl:rotate-180  w-3 h-3 mx-1 text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+                <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">
+                  {params.courseId}
+                </span>
+              </div>
+            </li>
+            <li aria-current="page">
+              <div class="flex items-center">
+                <svg
+                  class="rtl:rotate-180  w-3 h-3 mx-1 text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+                <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">
+                  livestream
+                </span>
+              </div>
+            </li>
+          </ol>
+        </nav>
+        {/* Middle Card */}
+        <div className="grid stream-grid p-2">
+          <video
+            className="w-full h-full p-3 rounded-md" // Ensure rounded corners
+            playsInline
+            ref={myVideoRef}
+            autoPlay
+            muted
+          />
+          <div className="mt-4">
+            <Card className="w-[350px] h-[500px] flex flex-col">
+              <CardHeader>
+                <CardTitle>Live Chat</CardTitle>
+                <CardDescription>
+                  Engage with your viewers in real-time.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="flex-1 overflow-y-auto">
+                {messages.length === 0 ? (
+                  <div className="text-center text-gray-500">
+                    No messages yet. Be the first to chat!
+                  </div>
+                ) : (
+                  messages.map((msg, index) => (
+                    <div
+                      key={index}
+                      className="p-2 mb-2 bg-gray-100 rounded-md text-sm"
+                    >
+                      {msg}
+                    </div>
+                  ))
+                )}
+              </CardContent>
+              <CardFooter className="mt-auto">
+                <div className="grid w-full gap-2">
+                  <Textarea
+                    placeholder="Type your message here."
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                  />
+                  <Button onClick={handleSendMessage}>Send message</Button>
+                </div>
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
+        {/* Bottom Cards */}
+        <div>
+          <div className="w-full p-5 bg-gray-100 h-[80px]">
+            <h1 className="text-xl text-left">Fundamentals of Computer Science</h1>
+            
+          </div>
+          <Tabs
+            defaultValue="agenda"
+            className="w-full p-2 bg-gray-100 h-[200px]"
+          >
+            <TabsList className="grid w-full grid-cols-4">
+              <TabsTrigger value="announcement">
+                Announcement{" "}
+                <span class="inline-flex items-center justify-center w-4 h-4 ms-2 text-xs font-semibold text-red-800 bg-red-200 rounded-full">
+                  1
+                </span>
+              </TabsTrigger>
+              <TabsTrigger value="agenda">Agenda</TabsTrigger>
+              <TabsTrigger value="notes">Notes</TabsTrigger>
+              <TabsTrigger value="doubt">Raise a Doubt</TabsTrigger>
+            </TabsList>
+            <TabsContent value="announcement" className="p-2">
+              No Announcements from tutor yet!
+            </TabsContent>
+            <TabsContent value="agenda" className="p-2">
+              Live stream agenda will be displayed here.
+            </TabsContent>
+            <TabsContent value="notes" className="p-2">Take down notes</TabsContent>
+            <TabsContent value="doubt" className="p-2">Ask a question from tutor</TabsContent>
+          </Tabs>
+        </div>
+      </div>
     </div>
   );
 }
