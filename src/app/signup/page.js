85f3979802d9edbc89e386ef3fb1a4f2453eaa93
Ashwin Tauro <ashwinshawntauro@gmail.com>
2024-11-10 11:00:49 +0530

    style:signup with roles

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { useState, useEffect, useContext } from "react";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import {
@@ -62,11 +63,7 @@ export default function Page() {
     setError(null);
 
     try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        pass
-      );
+      const userCredential = await createUserWithEmailAndPassword(auth, email, pass);
       const user = userCredential.user;
       let token = await getIdToken(user);
       setCookie(token);
@@ -78,7 +75,7 @@ export default function Page() {
     }
   };
 
-  const handleGoogle = async (event) => {
+  const handleGoogle = async () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
@@ -90,8 +87,7 @@ export default function Page() {
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        setError(errorCode + errorMessage);
+        setError(`${errorCode} ${errorMessage}`);
       });
   };
 
@@ -104,119 +100,146 @@ export default function Page() {
           </h2>
         </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <span className="flex items-center justify-center">
-            <button onClick={handleGoogle} className="flex items-center border hover:bg-primary-light border-blue-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800">
-              <svg class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                <path fill-rule="evenodd" d="M12.037 21.998a10.313 10.313 0 0 1-7.168-3.049 9.888 9.888 0 0 1-2.868-7.118 9.947 9.947 0 0 1 3.064-6.949A10.37 10.37 0 0 1 12.212 2h.176a9.935 9.935 0 0 1 6.614 2.564L16.457 6.88a6.187 6.187 0 0 0-4.131-1.566 6.9 6.9 0 0 0-4.794 1.913 6.618 6.618 0 0 0-2.045 4.657 6.608 6.608 0 0 0 1.882 4.723 6.891 6.891 0 0 0 4.725 2.07h.143c1.41.072 2.8-.354 3.917-1.2a5.77 5.77 0 0 0 2.172-3.41l.043-.117H12.22v-3.41h9.678c.075.617.109 1.238.1 1.859-.099 5.741-4.017 9.6-9.746 9.6l-.215-.002Z" clip-rule="evenodd" />
-              </svg>
+          <Tabs value={role} onValueChange={setRole} className="space-y-6">
+            <TabsList className="flex space-x-2">
+              <TabsTrigger value="student" className="w-full">Student</TabsTrigger>
+              <TabsTrigger value="tutor" className="w-full">Tutor</TabsTrigger>
+            </TabsList>
 
-              <span className="px-2">Continue with Google</span>
-            </button>
-          </span>
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            {error && <p className="text-red-500">{error}</p>}
-            <div>
-              <Label
-                htmlFor="name"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Name
-              </Label>
-              <div className="mt-2">
-                <Input
-                  id="name"
-                  name="name"
-                  type="string"
-                  autoComplete="name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <Label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Email address
-              </Label>
-              <div className="mt-2">
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="flex items-center justify-between">
-                <Label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </Label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-primary">
-                    Forgot password?
-                  </a>
+            <TabsContent value="student">
+              <form className="space-y-6" onSubmit={handleSubmit}>
+                {error && <p className="text-red-500">{error}</p>}
+                <span className="flex items-center justify-center">
+                  <button onClick={handleGoogle} className="flex items-center border hover:bg-primary-light border-blue-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800">
+                    <svg class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                      <path fillRule="evenodd" d="M12.037 21.998a10.313 10.313 0 0 1-7.168-3.049 9.888 9.888 0 0 1-2.868-7.118 9.947 9.947 0 0 1 3.064-6.949A10.37 10.37 0 0 1 12.212 2h.176a9.935 9.935 0 0 1 6.614 2.564L16.457 6.88a6.187 6.187 0 0 0-4.131-1.566 6.9 6.9 0 0 0-4.794 1.913 6.618 6.618 0 0 0-2.045 4.657 6.608 6.608 0 0 0 1.882 4.723 6.891 6.891 0 0 0 4.725 2.07h.143c1.41.072 2.8-.354 3.917-1.2a5.77 5.77 0 0 0 2.172-3.41l.043-.117H12.22v-3.41h9.678c.075.617.109 1.238.1 1.859-.099 5.741-4.017 9.6-9.746 9.6l-.215-.002Z" clipRule="evenodd" />
+                    </svg>
+
+                    <span className="px-2">Continue with Google</span>
+                  </button>
+                </span>
+                <h6 className="flex justify-center font-semibold">OR</h6>
+                <div>
+                  <Label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                    Name
+                  </Label>
+                  <div className="mt-2">
+                    <Input
+                      id="name"
+                      name="name"
+                      type="string"
+                      autoComplete="name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      required
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="mt-2">
-                <Input
-                  id="password"
-                  name="password"
-                  type="password"
-                  value={pass}
-                  onChange={(e) => setPass(e.target.value)}
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
-                />
-              </div>
-              <div className="inline-flex mt-2">
-                <Label htmlFor="regfor" className="mr-2 mt-2">
-                  Register as:{" "}
-                </Label>
-                <RadioGroup
-                  id="regfor"
-                  className="m-2 flex"
-                  value={role}
-                  onValueChange={setRole}
-                >
-                  <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="student" id="student" />
-                    <Label htmlFor="student">Student</Label>
+                <div>
+                  <Label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                    Email address
+                  </Label>
+                  <div className="mt-2">
+                    <Input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
                   </div>
-                  <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="tutor" id="tutor" />
-                    <Label htmlFor="tutor">Tutor</Label>
+                </div>
+                <div>
+                  <Label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                    Password
+                  </Label>
+                  <div className="mt-2">
+                    <Input
+                      id="password"
+                      name="password"
+                      type="password"
+                      value={pass}
+                      onChange={(e) => setPass(e.target.value)}
+                      autoComplete="current-password"
+                      required
+                    />
                   </div>
-                </RadioGroup>
-              </div>
-            </div>
-            <div>
-              <Button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white"
-              >
-                Register
-              </Button>
-            </div>
-          </form>
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Already a member?
-            <a href="/signin" className="font-semibold leading-6 px-1">
-              Sign In
-            </a>
-          </p>
+                </div>
+                <div>
+                  <Button type="submit" className="w-full">Register as Student</Button>
+                </div>
+              </form>
+            </TabsContent>
+
+            <TabsContent value="tutor">
+              <form className="space-y-6" onSubmit={handleSubmit}>
+                <span className="flex items-center justify-center">
+                  <button onClick={handleGoogle} className="flex items-center border hover:bg-primary-light border-blue-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800">
+                    <svg className="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                      <path fill-rule="evenodd" d="M12.037 21.998a10.313 10.313 0 0 1-7.168-3.049 9.888 9.888 0 0 1-2.868-7.118 9.947 9.947 0 0 1 3.064-6.949A10.37 10.37 0 0 1 12.212 2h.176a9.935 9.935 0 0 1 6.614 2.564L16.457 6.88a6.187 6.187 0 0 0-4.131-1.566 6.9 6.9 0 0 0-4.794 1.913 6.618 6.618 0 0 0-2.045 4.657 6.608 6.608 0 0 0 1.882 4.723 6.891 6.891 0 0 0 4.725 2.07h.143c1.41.072 2.8-.354 3.917-1.2a5.77 5.77 0 0 0 2.172-3.41l.043-.117H12.22v-3.41h9.678c.075.617.109 1.238.1 1.859-.099 5.741-4.017 9.6-9.746 9.6l-.215-.002Z" clip-rule="evenodd" />
+                    </svg>
+
+                    <span className="px-2">Continue with Google</span>
+                  </button>
+                </span>
+                {error && <p className="text-red-500">{error}</p>}
+                <h6 className="flex justify-center font-semibold">OR</h6>
+                <div>
+                  <Label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                    Name
+                  </Label>
+                  <div className="mt-2">
+                    <Input
+                      id="name"
+                      name="name"
+                      type="string"
+                      autoComplete="name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      required
+                    />
+                  </div>
+                </div>
+                <div>
+                  <Label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                    Email address
+                  </Label>
+                  <div className="mt-2">
+                    <Input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
+                  </div>
+                </div>
+                <div>
+                  <Label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                    Password
+                  </Label>
+                  <div className="mt-2">
+                    <Input
+                      id="password"
+                      name="password"
+                      type="password"
+                      value={pass}
+                      onChange={(e) => setPass(e.target.value)}
+                      autoComplete="current-password"
+                      required
+                    />
+                  </div>
+                </div>
+                <div>
+                  <Button type="submit" className="w-full">Register as Tutor</Button>
+                </div>
+              </form>
+            </TabsContent>
+          </Tabs>
         </div>
       </div>
     </>
