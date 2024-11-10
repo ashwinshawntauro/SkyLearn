cd806f9ce29fc7e9bfb81f823c7d9f9d159e6f56
Ashwin Tauro <ashwinshawntauro@gmail.com>
2024-11-10 22:18:25 +0530

    functionality:profile page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'ui-avatars.com',
+            pathname: '/api/**', // matches the path pattern for avatars
+          },
+        ],
+      },
 };
 
 export default nextConfig;
