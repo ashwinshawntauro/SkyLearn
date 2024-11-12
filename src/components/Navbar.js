6fe8eb867c3ee703a7e32a357ab52311de34a694
Ashwin Tauro <ashwinshawntauro@gmail.com>
2024-11-12 13:30:42 +0530

    fix:signup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
     return (
         <div>
             <header className="flex items-center justify-between p-2 bg-white shadow max-h-[50px]">
-                <Button className="text-black bg-white hover:text-white group" onClick={(e)=>router.push('/')}>
+                <Button className="text-black bg-white hover:text-white group" onClick={(e)=>router.replace('/')}>
                     <svg className="w-6 h-6 text-gray-800 group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                         <path fillRule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clipRule="evenodd" />
                     </svg>
@@ -43,7 +43,7 @@ function Navbar() {
                         </svg>
                     </button>
 
-                    <Avatar className="w-[30px] cursor-pointer" onClick={(e) => router.push('/profile')}>
+                    <Avatar className="w-[30px] cursor-pointer" onClick={(e) => router.replace('/profile')}>
                         <AvatarImage className="rounded-full" src={`https://ui-avatars.com/api/?name=${userName}&background=1e90ff&color=FFFFFF`} />
                         <AvatarFallback>{userName}</AvatarFallback>
                     </Avatar>
